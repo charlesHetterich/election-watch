409a684f5977ef29c5110cc9a50053113951e654
chetterich <hetterich.charles@gmail.com>
2025-04-19 01:03:21 +0200

    readme and cleanup

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Super light weight app to run as a free fly.io session that watches for phase ch
 Currently this simply sends an email. In the future I would like this to trigger a slightly larger process to download the election snapshot and add it to a huggingface dataset. For right now, I am the slightly larger process (aka manual style).
 
 In the future this will also spin up & shut down a server to calculate & submit my solutions.
+
+*An interesting slightly more ambitious idea in the future could be to to use this concept to build a general 'lambda' type SaaS that triggers lambda functions on any polkadot event*
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,68 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 // `dot` is the name we gave to `npx papi add`
-import { dot } from "@polkadot-api/descriptors"
-import { createClient } from "polkadot-api"
+import { dot } from "@polkadot-api/descriptors";
+import { createClient } from "polkadot-api";
 import { getSmProvider } from "polkadot-api/sm-provider";
 import { chainSpec } from "polkadot-api/chains/polkadot";
 import { start } from "polkadot-api/smoldot";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config();
-
-const EMAIL = process.env.EMAIL;
-const PASSWORD = process.env.PASSWORD;
-
-if (!EMAIL || !PASSWORD) {
-    throw new Error('EMAIL and PASSWORD must be set in the .env file');
-}
-
+// setup PAPI
 const smoldot = start();
 const chain = await smoldot.addChain({ chainSpec });
+const client = createClient(getSmProvider(chain));
+const papi = client.getTypedApi(dot);
 
-// Connect to the polkadot relay chain.
-const client = createClient(
-    getSmProvider(chain)
-);
-
-// With the `client`, you can get information such as subscribing to the last
-// block to get the latest hash:
-
-// To interact with the chain, you need to get the `TypedApi`, which includes
-// all the types for every call in that chain:
-const dotApi = client.getTypedApi(dot)
-
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: EMAIL,
-        pass: PASSWORD,
-    },
+// setup gmail access
+dotenv.config();
+const EMAIL = process.env.EMAIL;
+const PASSWORD = process.env.PASSWORD;
+if (!EMAIL || !PASSWORD) {
+  throw new Error("EMAIL and PASSWORD must be set in the .env file");
+}
+const gmailAcc = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: EMAIL,
+    pass: PASSWORD,
+  },
 });
 
-function emailData(data: any) {
-    const dataString = typeof data === 'object' && data !== null
-        ? JSON.stringify(data, (key, value) =>
-            typeof value === 'bigint' ? value.toString() : value, 2
+/**
+ * Sends data to the specified email address.
+ *
+ * @param {any} content - The content to be sent in the email.
+ * @returns {void}
+ */
+function sendToEmail(content: any) {
+  const dataString =
+    typeof content === "object" && content !== null
+      ? JSON.stringify(
+          content,
+          (key, value) =>
+            typeof value === "bigint" ? value.toString() : value,
+          2
         )
-        : String(data);
+      : String(content);
 
-    const mailOptions = {
-        from: EMAIL,
-        to: EMAIL,
-        subject: 'Test Email',
-        text: dataString,
-    };
+  const mailOptions = {
+    from: EMAIL,
+    to: EMAIL,
+    subject: "DELIVERY HONK",
+    text: dataString,
+  };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Error sending email:', error);
-        } else {
-            console.log('Email sent:', info.response);
-        }
-    });
+  gmailAcc.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+    } else {
+      console.log("Email sent:", info.response);
+    }
+  });
 }
 
-dotApi.event.ElectionProviderMultiPhase.PhaseTransitioned.watch((data) => true)
-    .forEach(emailData)
-
-// dotApi.event.NominationPools.Bonded.watch((data) => true)
-//     .forEach(emailData)
+// listen for phase transitions
+papi.event.ElectionProviderMultiPhase.PhaseTransitioned.watch(
+  (data) => true
+).forEach(sendToEmail);
