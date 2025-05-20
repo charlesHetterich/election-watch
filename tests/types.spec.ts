8b862c00989bfcb09d684a81fb406ad521dac539
chetterich <hetterich.charles@gmail.com>
2025-05-20 14:35:36 -0500

    unit testing for types scaffolding

---
+++ tests/types.spec.ts
@@ -0,0 +1,20 @@
+// TODO! Unit test validation of all of our type-hint infrastructure
+import {
+    beforeAll,
+    beforeEach,
+    describe,
+    expect,
+    it,
+    MockInstance,
+    vi,
+} from "vitest";
+
+describe("Substrate Lambdas type system", async () => {
+    describe("known-chains", async () => {
+        it("should ...", async () => {});
+    });
+
+    describe("Contextualized Apps", async () => {
+        it("should ...", async () => {});
+    });
+});
